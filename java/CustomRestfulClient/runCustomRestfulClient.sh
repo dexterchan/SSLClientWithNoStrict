c782da7ca5f8685c67d4824ea992cda46a89bd58
dexter <dexter.chan@outlook.com>
2016-06-12 12:57:50 +0800

    update shell cmd

--- java/CustomRestfulClient/runCustomRestfulClient.sh
+++ java/CustomRestfulClient/runCustomRestfulClient.sh
@@ -1,4 +1,4 @@
 export title=testing
 export description=funny description
 export host=192.168.88.1
-java -jar ./out/CustomRestfulClient.jar --name="Spring" -MODE POST -MAXTRY 3 -ENV_VAR title,description -URL https://$host:443/todo/api/v1.0/tasks
+java -jar ./target/CustomRestfulClient-0.0.1.jar --name="Spring" -MODE POST -MAXTRY 3 -ENV_VAR title,description -URL https://$host:443/todo/api/v1.0/tasks
