655c0ca3d883173ead767fd030436c78110c53a6
dexter <dexter.chan@outlook.com>
2016-06-12 22:51:56 +0800

    update Linux run

--- java/CustomRestfulClient/runCustomRestfulClient.sh
+++ java/CustomRestfulClient/runCustomRestfulClient.sh
@@ -1,4 +1,6 @@
 export title=testing
 export description=funny description
 export host=192.168.88.1
-java -jar ./target/CustomRestfulClient-0.0.1.jar --name="Spring" -MODE POST -MAXTRY 3 -ENV_VAR title,description -URL https://$host:443/todo/api/v1.0/tasks
+#java -jar ./target/CustomRestfulClient-0.0.1.jar --name="Spring" -MODE POST -MAXTRY 3 -ENV_VAR title,description -URL https://$host:443/todo/api/v1.0/tasks
+
+java -jar ./target/CustomRestfulClient-0.0.1.jar --name="Spring" -RUNTEMPL ToDoWebActionTemplate -ACTION insert
